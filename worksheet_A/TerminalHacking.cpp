ccde6f42bb9e6219d1676cd7c8900fbdc61ff6fb
John Rock <rasta_panda@hotmail.co.uk>
2017-01-23 10:24:59 +0000

    Added a lit set of code

--- worksheet_A/TerminalHacking.cpp
+++ worksheet_A/TerminalHacking.cpp
@@ -40,7 +40,68 @@ int main()
 	}
 
 	// TODO: implement the rest of the game
+	
+	string guessedWord;
+	int likeness = 0;
+	int guesses = 0;
+	
+	//<------------------------------------------------------------
+	//Probably won't work here but is working code.
+	cout << "\nHow many letters do you want per word? 4-7\n";
+    cin>>opt; //Fetches user response
+         
+         switch (opt) //selection statement - option
+         {
+                case '4': //Condition
+                      wordLength = 4;//Changes variable controlling word length
+                     break; //Stops conditions overlapping
+                case '5':
+                     wordLength = 5;
+                     break;
+                case '6':
+                     wordLength = 6;
+                     break;
+                case '7':
+                     wordLength = 7;
+                     break;
 
-    return 0;
+//----------------------------------------------------------------->
+
+	while (guesses < 4)
+	{
+		cout << "\nPlease enter your guessed word in all capitals.:\n";
+		cin >> guessedWord;
+
+		set<string>iterator i = options.find(guessedWord);
+		if (i == options.end())
+		{
+		cout << "Invalid Guess." << endl;
+		continue;
+		}
+		else
+		{
+			cout << "\nCalculating Score.\n";
+		}
+
+		likeness = getLikeness(guessedWord, secret);
+		cout << "\nScore: " << likeness << " out of " << wordLength << "\n";
+
+		if (likeness == wordLength)
+		{
+			cout << "\nYou guessed correctly!\n";
+			exit(0);
+		}
+
+		else
+		{
+			guesses++;
+			cout << "\nYou have " << 4 - guesses << " guesses remaining. \n";
+			likeness = 0;
+		}
+
+	}
+
+	cout << "\nYou lose!\n";
+
+  return 0;
 }
-
